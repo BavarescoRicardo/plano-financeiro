f49bf2dc3ba1b975a99310687fbe57d928ef0bbc
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2024-03-02 15:48:56 -0300

    rotina para index / com hbs

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
 
 app.useStaticAssets(join(__dirname, '..', 'public'));
-app.setBaseViewsDir(join(__dirname, '..', 'views'));
+app.setBaseViewsDir(join(__dirname, '/src/', 'views'));
 app.setViewEngine('hbs');
 }
 
